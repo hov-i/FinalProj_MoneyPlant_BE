3a4c6a38de4b51a6458fe64655ac9d1cf86c22d9
Soomin <o_v_zzza@naver.com>
2023-06-23 10:09:58 +0900

    ✨ work, schedule dto 생성

---
+++ src/main/java/com/MoneyPlant/dto/ScheduleDto.java
@@ -0,0 +1,16 @@
+package com.MoneyPlant.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScheduleDto {
+
+//    private Long scId;
+    private Long userId; // join 으로 사용 예정
+    private String calId;
+    private String workName;
+    private String payType;
+    private String scBudget;
+
+}
--- src/main/java/com/MoneyPlant/entity/Schedule.java
+++ src/main/java/com/MoneyPlant/entity/Schedule.java
@@ -1,2 +1,36 @@
-package com.MoneyPlant.entity;public class Schedule {
+package com.MoneyPlant.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="schedule")
+@Setter
+@Getter
+@ToString
+public class Schedule { // sc을 약어로 사용합니다.
+    @Id
+    @Column(name = "sc_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long scId; // 일정 Id
+
+    @ManyToOne
+    @JoinColumn(name = "id")
+    private User user;
+
+    @Column(name = "calendar_id", unique = true)
+    private String calId; // 구글 캘린더 ID
+
+    @Column(name = "sc_name", nullable = false)
+    private String workName; // 일정 이름
+
+    @Column(name = "sc_date", nullable = false)
+    private String payType; // 일정 날짜
+
+    @Column(name = "sc_budget")
+    private String scBudget; // 일정 예산
+
 }
