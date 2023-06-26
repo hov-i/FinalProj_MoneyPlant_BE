cbe634c6b1dc6534795fcd6b80910b5847b86bfb
Soomin <o_v_zzza@naver.com>
2023-06-27 00:19:47 +0900

    ✨ MyPage, 캘린더 일정&근무 기능 추가

---
+++ src/main/java/com/MoneyPlant/controller/MyScheduleController.java
@@ -0,0 +1,5 @@
+package com.MoneyPlant.controller;
+
+public class MyScheduleController {
+
+}
--- src/main/java/com/MoneyPlant/controller/WorkController.java
+++ src/main/java/com/MoneyPlant/controller/WorkController.java
@@ -1,8 +1,8 @@
 package com.MoneyPlant.controller;
 
-import com.MoneyPlant.dto.ScheduleDto;
-import com.MoneyPlant.entity.Schedule;
-import com.MoneyPlant.service.ScheduleService;
+import com.MoneyPlant.dto.WorkDto;
+import com.MoneyPlant.entity.Work;
+import com.MoneyPlant.service.WorkService;
 import com.MoneyPlant.service.jwt.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,25 +18,24 @@ import javax.servlet.http.HttpServletRequest;
 @RestController
 @Slf4j
 @CrossOrigin(origins = "https://localhost:3000", allowedHeaders = "*")
-@RequestMapping("/schedule")
+@RequestMapping("/work")
 @RequiredArgsConstructor
-public class ScheduleController {
+public class WorkController {
     @Autowired
-    ScheduleService scheduleService;
+    WorkService workService;
 
     @PostMapping
-    public ResponseEntity<String> createSchedule(
-            @RequestBody ScheduleDto scheduleDto,
+    public ResponseEntity<String> createWork(
+            @RequestBody WorkDto workDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        boolean isSuccess = scheduleService.createSchedule(scheduleDto, userDetails);
+        boolean isSuccess = workService.createWork(workDto, userDetails);
 
         if (isSuccess) {
-            return ResponseEntity.ok("일정이 생성되었습니다.");
+            return ResponseEntity.ok("근무가 생성되었습니다.");
         } else {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("일정 생성을 실패했습니다.");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("근무 생성을 실패했습니다.");
         }
     }
-
 
 
 }
