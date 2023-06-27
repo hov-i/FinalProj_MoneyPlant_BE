549a5bfbb51d20d908b44da0384856a47210872e
Soomin <o_v_zzza@naver.com>
2023-06-28 00:53:53 +0900

    🐛 Dto 수정

--- src/main/java/com/MoneyPlant/dto/MyWorkDto.java
+++ src/main/java/com/MoneyPlant/dto/MyWorkDto.java
@@ -7,8 +7,10 @@ import lombok.Setter;
 @Setter
 public class MyWorkDto {
     private Long userId; // join 으로 사용 예정
-    private String myPayType;
     private String myWorkName;
+    private String myWorkColor;
+    private String myPayType;
+
     private int myWorkTime;
     private int myWorkPay;
     private int myPayDay;
--- src/main/java/com/MoneyPlant/dto/WorkDto.java
+++ src/main/java/com/MoneyPlant/dto/WorkDto.java
@@ -9,8 +9,9 @@ public class WorkDto {
 
 //    private Long workId;
     private Long userId; // join 으로 사용 예정
-    private String payType;
     private String workName;
+    private String workColor;
+    private String payType;
     private String workDate;
     private int workTime;
     private int workPay;
