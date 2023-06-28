eed736ea5a64c958ae27440e4270481e969f5cbb
lamsasa <dh85311@gmail.com>
2023-06-28 10:10:17 +0900

    ✨ ledger, list 완료

--- src/main/java/com/MoneyPlant/repository/ListRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.MoneyPlant.repository;
-//얘 키니까 오류남??
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import java.util.List;
-//
-//public interface ListRepository extends JpaRepository<List, Long> {
-//
-//}
