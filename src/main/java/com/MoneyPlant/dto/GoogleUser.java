8082f2852bb1cffc57212e07f4cf01e2265273bb
nocxel <wlrudnjtkddn@naver.con>
2023-07-04 12:54:33 +0900

    🐛 구글 로그인 오류 수정  dto 생성자 추가

--- src/main/java/com/MoneyPlant/dto/GoogleUser.java
+++ src/main/java/com/MoneyPlant/dto/GoogleUser.java
@@ -3,8 +3,11 @@ package com.MoneyPlant.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+@NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
